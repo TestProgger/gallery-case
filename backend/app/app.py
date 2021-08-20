c47be5930794563dc927e073ee83167e9131701a
TestProgger <gamersteam_dubki@mail.ru>
2021-08-20 22:07:13 +0300

    Added init_db route

--- backend/app/app.py
+++ backend/app/app.py
@@ -4,14 +4,28 @@ from fastapi import FastAPI
 from .model_handers import DeviceInfo , Vendor
 from .dto_types import DeviceInfoDTO
 
+from json import load
+
 app = FastAPI()
 
 
-@app.get("/{id}")
-async def plug(id : int):
-    return await DeviceInfo.get_by_id(id)
+@app.get("/init_db")
+async def init_db():
+    with open("./oui_data_m1.json" , "r") as jr:
+        vendors = load(jr)
 
+    vendor_names = list(vendors.keys())
 
+    for vendor_name  in vendor_names:
+        await Vendor.create(
+            Vendor.CreateT(
+                name = vendor_name,
+                ouis = [ Vendor.OUI( oui )  for oui in vendors[vendor_name]['macAdresses'] ],
+                activity_types= [ Vendor.ActivityType( at )  for at in vendors[vendor_name]['activityType'] ]
+            )
+        )
+
+    return {'a' : 111}
 
 @app.post("/create_device")
 async def create_device(dev :  DeviceInfoDTO):
