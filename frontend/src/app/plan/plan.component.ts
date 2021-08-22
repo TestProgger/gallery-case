26949e397156fe2e79674bf1137433a1e4ca8027
itkachenko <nel18@yandex.ru>
2021-08-22 09:51:01 +0300

    planned page

--- frontend/src/app/plan/plan.component.ts
+++ frontend/src/app/plan/plan.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Chart } from 'angular-highcharts';
 
 @Component({
   selector: 'app-plan',
@@ -10,6 +11,76 @@ export class PlanComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  chart = new Chart({
+    chart: {
+      type: 'column'
+    },
+    title: {
+      text: 'График показов билборда по часам'
+    },
+    credits: {
+      enabled: false
+    },
+    xAxis: {
+      categories: [
+        '1',
+        '2',
+        '3',
+        '4',
+        '5',
+        '6',
+        '7',
+        '8',
+        '9',
+        '10',
+        '11',
+        '12',
+        '13',
+        '14',
+        '15',
+        '16',
+        '17',
+        '18',
+        '19',
+        '20',
+        '21',
+        '22',
+        '23',
+        '24',
+      ],
+      crosshair: true,
+      title: {
+        text: 'Количество'
+      }
+    },
+    yAxis: {
+      min: 0,
+      title: {
+        text: 'Показы'
+      }
+    },
+    tooltip: {
+      headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+      pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+        '<td style="padding:0"><b>{point.y:.1f}</b></td></tr>',
+      footerFormat: '</table>',
+      shared: true,
+      useHTML: true
+    },
+    series: [
+      {
+        name: 'NVS002APL',
+        type: 'column',
+        data: [9,9,9,9,9,9,18,18,18,18,18,24,24,18,18,24,24,24,24,18,18,18,9,9]
+      }
+    ]
+  });
+
+  // add point to chart serie
+  add() {
+    this.chart.addPoint(Math.floor(Math.random() * 10));
   }
 
 }
